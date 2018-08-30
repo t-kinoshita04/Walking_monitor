d8247b5c48e5d84f4b7f0004d7abd8845483b6d9
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2018-08-30 14:51:32 +0900

    3画面が完成

---
+++ Assets/Dropdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // LoadSceneを使うために必要!!
+
+public class Dropdown : MonoBehaviour
+{
+    public void OnValueChanged(UnityEngine.UI.Dropdown dropdown)
+    {
+        switch (dropdown.value)
+        {
+            case 0:
+                SceneManager.LoadScene("HomeScene");
+                break;
+            case 1:
+                SceneManager.LoadScene("EvaluateScene");
+                break;
+            case 2:
+                SceneManager.LoadScene("DataScene");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/MonitorDirector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  // LoadSceneを使うために必要!!
+
+public class MonitorDirector : MonoBehaviour {
+    // ボタンをクリックするとEvaluateSceneに移動します
+    public void ButtonClicked(int number)
+    {
+        switch (number)
+        {
+            case 0:
+                SceneManager.LoadScene("EvaluateScene");
+                break;
+            case 1:
+                SceneManager.LoadScene("DataScene");
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
