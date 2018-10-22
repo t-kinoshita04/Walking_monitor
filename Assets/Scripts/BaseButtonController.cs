d0a6b52fc73d59a14ec74eff4f1720dfadfe0eaf
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2018-10-22 19:05:36 +0900

    スクロールビューのボタンクリックができるようにした（作業途中）

---
+++ Assets/Scripts/BaseButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseButtonController : MonoBehaviour {
+
+	public BaseButtonController button;
+
+	public void OnClick ()
+	{
+		if (button == null)
+		{
+			throw new System.Exception("Button instance is null!");
+		}
+		button.OnClick(this.gameObject.name);
+	}
+	
+	protected virtual void OnClick (string objectName) {
+		Debug.Log("Base Button");
+	}
+}
---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : BaseButtonController {
+
+	protected override void OnClick(string objectName) {
+		if ("Button1".Equals(objectName))
+		{
+			this.Button1Click();
+		}
+		else if ("Button2".Equals(objectName))
+		{
+			this.Button2Click();
+		}
+		else if ("Button3".Equals(objectName))
+		{
+			this.Button3Click();
+		}
+		else if ("Button4".Equals(objectName))
+		{
+			this.Button4Click();
+		}
+		else if ("Button5".Equals(objectName))
+		{
+			this.Button5Click();
+		}
+		else
+		{
+			throw new System.Exception("Not implemented!");
+		}
+	}
+	
+	private void Button1Click ()
+	{
+		Debug.Log("Button1 Click");
+	}
+
+	private void Button2Click ()
+	{
+		Debug.Log("Button2 Click");
+	}
+
+	private void Button3Click ()
+	{
+		Debug.Log("Button3 Click");
+	}
+
+	private void Button4Click ()
+	{
+		Debug.Log("Button4 Click");
+	}
+
+	private void Button5Click ()
+	{
+		Debug.Log("Button5 Click");
+	}
+}
