79785f43a587c4a6098d7a671cf09b1d19677539
TTCAD\t-kinoshita <t-kinoshita@ttc-net.co.jp>
2018-11-05 10:57:04 +0900

    現時点での作業終了

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -25,6 +25,42 @@ public class ButtonController : BaseButtonController {
 		{
 			this.Button5Click();
 		}
+		else if ("Button6".Equals(objectName))
+		{
+			this.Button6Click();
+		}
+		else if ("Button7".Equals(objectName))
+		{
+			this.Button7Click();
+		}
+		else if ("Button8".Equals(objectName))
+		{
+			this.Button8Click();
+		}
+		else if ("Button9".Equals(objectName))
+		{
+			this.Button9Click();
+		}
+		else if ("Button10".Equals(objectName))
+		{
+			this.Button10Click();
+		}
+		else if ("Button11".Equals(objectName))
+		{
+			this.Button11Click();
+		}
+		else if ("Button12".Equals(objectName))
+		{
+			this.Button12Click();
+		}
+		else if ("Button13".Equals(objectName))
+		{
+			this.Button13Click();
+		}
+		else if ("Button14".Equals(objectName))
+		{
+			this.Button14Click();
+		}
 		else
 		{
 			throw new System.Exception("Not implemented!");
@@ -54,5 +90,50 @@ public class ButtonController : BaseButtonController {
 	private void Button5Click ()
 	{
 		Debug.Log("Button5 Click");
+	}
+
+	private void Button6Click ()
+	{
+		Debug.Log("Button6 Click");
+	}
+
+	private void Button7Click ()
+	{
+		Debug.Log("Button7 Click");
+	}
+
+	private void Button8Click ()
+	{
+		Debug.Log("Button8 Click");
+	}
+
+	private void Button9Click ()
+	{
+		Debug.Log("Button9 Click");
+	}
+
+	private void Button10Click ()
+	{
+		Debug.Log("Button10 Click");
+	}
+
+	private void Button11Click ()
+	{
+		Debug.Log("Button11 Click");
+	}
+
+	private void Button12Click ()
+	{
+		Debug.Log("Button12 Click");
+	}
+
+	private void Button13Click ()
+	{
+		Debug.Log("Button13 Click");
+	}
+
+	private void Button14Click ()
+	{
+		Debug.Log("Button14 Click");
 	}
 }
